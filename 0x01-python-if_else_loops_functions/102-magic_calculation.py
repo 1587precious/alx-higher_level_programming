5bf685875be9135883873b22d8b628af91da283e
1587precious <preciousconsttance7@gmail.com>
2024-01-02 14:29:30 -0800

    task 16

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def magic_calculation(param_a, param_b, param_c):
+    if (param_a < param_b):
+        return (param_c)
+    elif (param_c > param_b):
+        return (param_a + param_b)
+    else:
+        return (param_a * param_b - param_c)
