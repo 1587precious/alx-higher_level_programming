1b3186d7ff25597551f4666cf19442ddeb8de8ee
1587precious <preciousconsttance7@gmail.com>
2024-01-06 13:20:36 -0800

    task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-def is_lower_case(c):
-    return 97 <= ord(c) <= 122
+def islower(c):
+    if ord(c) >= 97 and ord(c) <= 122:
+        return (True)
+    else:
+        return (False)
