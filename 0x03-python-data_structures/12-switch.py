1ba0593124ba4f11775a5d27a8f5d27748213516
1587precious <preciousconsttance7@gmail.com>
2024-01-06 10:01:33 -0800

    task 12

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-
-# Swap the values of 'a' and 'b'
 a = 89
 b = 10
-temp = a
-a = b
-b = temp
-
-# Display the swapped values
-print("a={} - b={}".format(a, b))
+a, b = b, a
+print("a={:d} - b={:d}".format(a, b))
