4c81105094716e64d407e41ea750d8774c13dab7
1587precious <preciousconsttance7@gmail.com>
2024-01-06 09:13:55 -0800

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
-def get_element(my_list, index):
-    if index < 0 or index >= len(my_list):
+def element_at(my_list, idx):
+    if idx < 0 or idx >= len(my_list):
         return None
     else:
-        return my_list[index]
+        return my_list[idx]
 
 # Example usage:
 # my_list = [1, 2, 3, 4, 5]
-# index = 2
-# result = get_element(my_list, index)
+# idx = 2
+# result = element_at(my_list, idx)
 # print(result)
