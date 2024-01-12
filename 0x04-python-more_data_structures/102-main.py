15ab92921e87c57fe419c0eb567b16b30413b80f
1587precious <preciousconsttance7@gmail.com>
2024-01-12 10:57:25 -0800

    task 15

---
+++ 0x04-python-more_data_structures/102-main.py
@@ -0,0 +1,27 @@
+a_dictionary = {'lang': "C", 'track': "Low", 'pref': "C", 'ids': [1, 2, 3]}
+new_dict = complex_delete(a_dictionary, 'C')
+print_sorted_dictionary(a_dictionary)
+print("--")
+print_sorted_dictionary(new_dict)
+
+print("--")
+print("--")
+new_dict = complex_delete(a_dictionary, 'c_is_fun')
+print_sorted_dictionary(a_dictionary)
+print("--")
+print_sorted_dictionary(new_dict)
+
+guillaume@ubuntu:~/0x04$ ./102-main.py
+ids: [1, 2, 3]
+track: Low
+--
+ids: [1, 2, 3]
+track: Low
+--
+--
+ids: [1, 2, 3]
+track: Low
+--
+ids: [1, 2, 3]
+track: Low
+guillaume@ubuntu:~/0x04$ 
