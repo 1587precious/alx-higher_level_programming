29195bc412276c972f1d38943ce02cf3ed9af8d4
1587precious <preciousconsttance7@gmail.com>
2024-01-12 10:19:35 -0800

    task 3

---
+++ 0x04-python-more_data_structures/3-main.py
@@ -0,0 +1,8 @@
+set_1 = { "Python", "C", "Javascript" }
+set_2 = { "Bash", "C", "Ruby", "Perl" }
+c_set = common_elements(set_1, set_2)
+print(sorted(list(c_set)))
+
+guillaume@ubuntu:~/0x04$ ./3-main.py
+['C']
+guillaume@ubuntu:~/0x04$ 
