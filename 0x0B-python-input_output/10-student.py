0621e8a38b4bcd1268013bfca44553b2d094550c
root <preciousconstance7@gmail.com>
2024-02-07 19:34:13 +0000

    input output

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,16 @@
+class Student:
+    """Defines a Student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance."""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: value for key, value in self.__dict__.items() if key in attrs}
+
