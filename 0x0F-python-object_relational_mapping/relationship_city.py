0bda5e66416b898d0f79dc03f342d2090cbbba07
1587precious <preciousconstance7@gmail.com>
2024-04-14 13:15:03 -0700

    task 15

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,28 +1,17 @@
 #!/usr/bin/python3
-
 """
-
-City class
-
+Contains the class definition of a City
 """
-
+from relationship_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import declarative_base
-
-Base = declarative_base()
+from sqlalchemy.ext.declarative import declarative_base
 
 
 class City(Base):
-    """The City class will abstract the implementation of City table"""
-    __tablename__ = "cities"
-
-    id = Column(Integer,
-            autoincrement=True,
-            primary_key=True,
-            unique=True,
-            nullable=False)
+    """
+    Class that defines each city
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
-
-    def __repr__(self):
-        return f"{self.id}: {self.name}"
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,26 +1,20 @@
 #!/usr/bin/python3
-
 """
-
-State class
-
+Contains State class and Base, an instance of declarative_base()
 """
-
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-from relationship_city import Base
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
 
 class State(Base):
-    """The State class will abstract the implementation of states table"""
-    __tablename__ = "states"
-
-    id = Column(Integer,
-            autoincrement=True,
-            primary_key=True,
-            unique=True,
-            nullable=False)
+    """
+    Class with id and name attributes of each state
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state")
-
-    def __repr__(self):
-        return f"{self.id}: {self.name}"
+    cities = relationship("City", backref="states")
