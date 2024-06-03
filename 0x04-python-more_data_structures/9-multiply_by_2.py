4af8a470958d72916bf204d2603896866199c060
1587precious <preciousconstance@gmail.com>
2024-06-03 12:32:33 -0700

    task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dictionary = a_dictionary.copy()
-    for key in new_dictionary:
-        new_dictionary[key] *= 2
-    return(new_dictionary)
+    return {key: val * 2 for key, val in a_dictionary.items()}
